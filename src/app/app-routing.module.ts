29da44732daa57c3ec56bdab1f5aab76417876a6
Arnav Vats <arnavvats97@gmail.com>
2019-05-11 14:17:07 +0530

    Added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {EmployeeListComponent} from './pages/employee-list/employee-list.component';
+import {EmployeeComponent} from './pages/employee/employee.component';
+import {EmployeeListService} from './pages/employee-list/employee-list.service';
+import {EmployeeService} from './pages/employee/employee.service';
+import {SkillsService} from './shared/services/skills.service';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'employee',
+    component: EmployeeComponent,
+    resolve: {employee: EmployeeService, skills: SkillsService}
+  }, {
+  path: 'employee-list',
+  component: EmployeeListComponent,
+    resolve: {employeeList: EmployeeListService, skills: SkillsService}
+  }, {
+    path: '**',
+    redirectTo: 'employee-list'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
